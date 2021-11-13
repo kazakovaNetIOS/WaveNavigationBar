cbc6c04709d6ccc6deac5f13bb29e52baa6b3e0d
Kazakova Natalia Vladimirovna <natkazakova@ozon.ru>
2021-11-13 11:46:10 +0300

    Initial Commit

---
+++ WaveNavigationBar/WaveNavigationBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveNavigationBarApp.swift
+//  WaveNavigationBar
+//
+//  Created by Kazakova Nataliya on 13.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct WaveNavigationBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
